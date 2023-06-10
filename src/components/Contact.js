b218d4f19782bf8fafecb6ce14bea5e0ab076459
Lasha <lashagoglidzelasha@gmail.com>
2023-06-10 22:02:10 +0400

    add map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,32 @@
-export default function Contact(){
+import React from 'react';
+import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 
+const containerStyle = {
+    width: '300px',
+    height: '300px',
+    margin: '0 auto',
+};
 
-    return <div style={{width:"300px", height:"300px",background:"green" , margin:"0 auto"}}></div>
+const center = {
+    lat: 41.721328406057054, // Replace with the latitude of the desired address
+    lng: 44.74276356828425, // Replace with the longitude of the desired address
+};
+
+const markerPosition = {
+    lat: 41.721328406057054,  // Replace with the latitude of the desired address
+    lng: 44.74276356828425, // Replace with the longitude of the desired address
+};
+
+function Contact() {
+    return (
+        <div style={containerStyle}>
+            <LoadScript googleMapsApiKey="AIzaSyAy9ysUYLoa4zg1J1pyAAtH5IV8clI0_zc">
+                <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+                    <Marker position={markerPosition} />
+                </GoogleMap>
+            </LoadScript>
+        </div>
+    );
 }
+
+export default Contact;
