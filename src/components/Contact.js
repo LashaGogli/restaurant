f7c925ae5e16c5f157301aecdc296f5f2670bb36
Lasha <lashagoglidzelasha@gmail.com>
2023-06-11 15:25:03 +0400

    contacts added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,41 @@
+import '../components/Contact.css'
 import React from 'react';
 import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 
 const containerStyle = {
-    width: '300px',
-    height: '300px',
-    margin: '0 auto',
+    width: '700px',
+    height: '500px',
+    // margin: "30px"
 };
 
 const center = {
-    lat: 41.721328406057054, // Replace with the latitude of the desired address
-    lng: 44.74276356828425, // Replace with the longitude of the desired address
+    lat: 41.721328406057054,
+    lng: 44.74276356828425,
 };
 
 const markerPosition = {
-    lat: 41.721328406057054,  // Replace with the latitude of the desired address
-    lng: 44.74276356828425, // Replace with the longitude of the desired address
+    lat: 41.721328406057054,
+    lng: 44.74276356828425,
 };
 
 function Contact() {
     return (
-        <div style={containerStyle}>
-            <LoadScript googleMapsApiKey="AIzaSyAy9ysUYLoa4zg1J1pyAAtH5IV8clI0_zc">
-                <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
-                    <Marker position={markerPosition} />
-                </GoogleMap>
-            </LoadScript>
+        <div className='bigDiv'>
+            <div style={containerStyle}>
+                <LoadScript googleMapsApiKey="AIzaSyAy9ysUYLoa4zg1J1pyAAtH5IV8clI0_zc">
+                    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+                        <Marker position={markerPosition} />
+                    </GoogleMap>
+                </LoadScript>
+            </div>
+            <div className='nextDiv'>
+                <p className='styledContacts'>Address: Kalistrate Kutateladze 17, Tbilisi, Georgia.</p>
+                <p className='styledContacts'>Phone Number: +995 588 345 543</p>
+                <p className='styledContacts'>Email: restaurant@gmail.com</p>
+
+            </div>
+
+
         </div>
     );
 }
